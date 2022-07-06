1b0b892d11bf7a800763b2e8930b36f4d3db7c69
klynsman <klynsman.lemos@gmail.com>
2022-07-06 12:40:37 -0300

    criando a função de adicionar zero e chamada a api

--- App.js
+++ App.js
@@ -6,6 +6,24 @@ import HitoryGraphic from './src/components/HistoryGraphic';
 import QuotationList from './src/components/QuotationList';
 import QuotationItem from './src/components/QuotationList/QuotationItem';
 
+function addZero(number){
+  if(number <= 9){
+    return "0" + number
+  }
+  return number
+}
+
+function url(qtdDay){
+  const date = new Date();
+  const listLastDay = qtdDay;
+  const endDate = `${date.getFullYear()}-${addZero(date.getMonth()+1)}-${addZero(date.getDate())}`;
+  date.setDate(data.getDate() - listLastDay)
+  const startDate = `${date.getFullYear()}-${addZero(date.getMonth()+1)}-${addZero(date.getDate())}`
+
+  return `https://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&send=${endDate}`
+
+}
+
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
